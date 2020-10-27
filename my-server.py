1c765517b910734da2c65f112c00c0c2a29f169d
Lonnie Souder II <lonnie2@souderfamily.net>
2020-10-27 18:35:38 -0400

    used template to render dynamic data

--- my-server.py
+++ my-server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, send_from_directory
+from flask import Flask, request, redirect, send_from_directory, render_template
 import csv
 
 highscores_path = 'highscores.csv'
@@ -14,13 +14,25 @@ def greeting(name):
 
 @app.route('/add-score/', methods=['POST', 'GET'])
 def add_score():
-    if request.method == 'POST':
+    if request.method == 'GET':
+        return send_from_directory('static', 'add-score.html')
+    elif request.method == 'POST':
         name = request.form['name']
         score = request.form['score']
         with open(highscores_path, 'a') as highscores:
             score_writer = csv.writer(highscores)
             score_writer.writerow([name, score])
-    return send_from_directory('static', 'add-score.html')
+        return redirect('/highscores/')
+
+@app.route('/highscores/')
+def highscores():
+    highscores = []
+    with open(highscores_path, 'r') as highscores_file:
+        highscores_csv = csv.reader(highscores_file)
+        for row in highscores_csv:
+            highscores.append(row)
+    highscores.sort(reverse=True, key=lambda entry: entry[1])
+    return render_template('highscores.html', highscores=highscores)
 
 if __name__ == '__main__':
     app.run(debug = True)
